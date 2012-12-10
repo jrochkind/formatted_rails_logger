008407fffbb78b88e056b5717178c604692cd9dc
Jonathan Rochkind <jonathan@dnil.net>
2012-12-10 14:35:57 -0500

    fix initial newline formatting
    \A instead of ^. Why did ^ used to work? I don't know. Also omit templating
    for logged lines consisting solely of whitespace. Newer versions of
    Rails do that, whereas older ones did it with prefixed newlines, bah.

--- lib/formatted_rails_logger/formatter.rb
+++ lib/formatted_rails_logger/formatter.rb
@@ -6,11 +6,20 @@ class FormattedRailsLogger::Formatter
   # or the Severity twice. 
   @@format_str = "[%s] %5s  %s"
   @@datetime_format = nil 
-  
-  def call(severity, time, progname, msg)   
+
+  def call(severity, time, progname, msg) 
+    msg = "" if msg.nil? # need to manipulate it as string
+
     # Rails 3.2 seems to automatically add a newline, for consistency
     # we will too. 
-    msg += "\n"
+    msg += "\n"    
+    
+    # if the msg is blank, meant to create a newline, then
+    # just output it without any preliminaries, to keep the
+    # newline as seperator. Also return quick for nil msg, to
+    # avoid causing problems with later logic. 
+    return msg if msg.blank?
+
     
     # see no need for micro-seconds like in Logger, milis suffices. 
     # No idea why documented %L and other such useful things
@@ -20,14 +29,15 @@ class FormattedRailsLogger::Formatter
     else
       time.strftime("%Y-%m-%d %H:%M:%S.") << time.usec.to_s[0..2].rjust(3)
     end
-        
+
     # Rails likes to log with some preceding newlines for spacing in the
     # logfile. We want to preserve those when present, 
     # but prefix actual content with our prefix.
-    msg = "" if msg.nil? # regexp won't like nil, but will be okay with ""
-    matchData = ( /^(\n*)/.match(msg)  )    
+    matchData = ( /\A(\n*)/.match(msg)  )    
+    
+
     matchData[0] + (@@format_str % [formatted_time, severity, matchData.post_match])
-  end    
-  
+  end
+
 end
 
